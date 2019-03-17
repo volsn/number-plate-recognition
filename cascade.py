470cb8b6a00f8d757ddfb99e1995450bdc2263a4
Volosnikov <volosnikovkolia@gmail.com>
2019-03-17 19:12:21 +0200

    Added OCR text recognition
    Now script can read text from car plate using tesseract and validate it if it is necessary

--- cascade.py
+++ cascade.py
@@ -3,15 +3,79 @@ import numpy as np
 from os import listdir, makedirs
 from os.path import isfile, join, exists
 import sys
+import pytesseract
 
 
+# Standarts of number plates for different countries
+PLATE_NUMBER_STANDARTS = {
+	'RU': {
+		'dig': 3,
+		'chr': 3,
+	},
+}
+
+
+# Error that occures when there is no input data 
 class NoInputDataError(Exception):
 	def __init__(self, message, errors):
 		super().__init__(message)
 		self.errors = errors
 
 
-def get_plate_num_images(files, show=False):
+def check_is_plate_valid(plate_number, country='RU'):
+	"""
+	Checks whether the given string may be a car plate. Russian numbers by default
+	:plate_number type: srting
+	:country type: string
+	:rtype: bool
+	"""
+	
+	digits = ''.join([c for c in plate_number if c.isdigit()])
+	letters =  ''.join([c for c in plate_number if c.isalpha()])
+	print(digits, letters)
+
+	if len(digits) >= PLATE_NUMBER_STANDARTS[country]['dig'] and \
+				len(letters) >= PLATE_NUMBER_STANDARTS[country]['chr']:
+		return True
+	return False
+
+
+def find_plate_number(img_name, validation=False):
+	"""
+	Tries to find text on a car plate
+	:img_name type: string
+	:validation type: bool
+	:rtype: string
+	"""
+
+	path = join('result', img_name)
+	img = cv2.imread(path)
+	im_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+	(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+	thresh = 127
+	im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+	edges = cv2.Canny(im_bw, 50, 150, apertureSize = 3)
+
+	config = ('-l rus --oem 1 --psm 6')
+	text = pytesseract.image_to_string(img, config=config)
+
+	if validation:
+		if check_is_plate_valid(text):
+			return text
+		else:
+			return 'Not a car plate'
+	return text
+
+def get_plate_num_images(files, show=False, validation=False):
+	"""
+	Finds all the potential car plates among given photos
+	:files type: list
+	:show type: bool
+	:validation type: bool
+	:rtype: True
+	"""
 
 	cascade = cv2.CascadeClassifier('licence_plate.xml')
 
@@ -29,27 +93,33 @@ def get_plate_num_images(files, show=False):
 		for (x,y,w,h) in number_plate:
 			croped_licence_plate = img[y:y+h, x:x+w]
 			cv2.imwrite(join('result', '{}.png'.format(result_file_name)), croped_licence_plate)
+			print(find_plate_number('{}.png'.format(result_file_name), validation=validation))
 			result_file_name += 1
+
 			if show:
 				cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
 
 		if show:
 			cv2.imshow('img', img)
 			k = cv2.waitKey(0)
-			
+
 	if show:
 		cv2.DestroyAllWindows()
 
 
 if __name__ == '__main__':
-
 	if not exists('input'):
 		raise NoInputDataError('Data to process should be stored in `input` folder')
 
 	files = [f for f in listdir('input') if isfile(join('input', f))]
 	
+	show = False
 	if 'show' in sys.argv:
+		show = True
 		print('Files to be processed: \n', files)
-		get_plate_num_images(files, show=True)
+	validation = False
+	if 'validate' in sys.argv:
+		validation = True
 
-	get_plate_num_images(files)
+
+	get_plate_num_images(files, show=show, validation=validation)
